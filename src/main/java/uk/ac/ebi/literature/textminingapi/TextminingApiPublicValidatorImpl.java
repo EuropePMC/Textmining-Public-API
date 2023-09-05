c8d2e6c079804d473bc0c7c6d49c096716aff791
Islam Mohamed Mostafa Hassan <ishassan@ebi.ac.uk>
2023-09-05 15:07:00 +0100

    Remove the check that the article is in Europe PMC

--- src/main/java/uk/ac/ebi/literature/textminingapi/TextminingApiPublicValidatorImpl.java
+++ src/main/java/uk/ac/ebi/literature/textminingapi/TextminingApiPublicValidatorImpl.java
@@ -1,7 +1,16 @@
 package uk.ac.ebi.literature.textminingapi;
 
-import java.net.URLEncoder;
-import java.time.Duration;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.literature.textminingapi.pojo.FileInfo;
+import uk.ac.ebi.literature.textminingapi.pojo.Status;
+import uk.ac.ebi.literature.textminingapi.pojo.SubmissionMessage;
+import uk.ac.ebi.literature.textminingapi.service.MongoService;
+import uk.ac.ebi.literature.textminingapi.utility.Utility;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -10,33 +19,14 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.validator.routines.UrlValidator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import reactor.util.retry.Retry;
-import uk.ac.ebi.literature.textminingapi.europepmcpojos.EuropePMCLite;
-import uk.ac.ebi.literature.textminingapi.pojo.FileInfo;
-import uk.ac.ebi.literature.textminingapi.pojo.Status;
-import uk.ac.ebi.literature.textminingapi.pojo.SubmissionMessage;
-import uk.ac.ebi.literature.textminingapi.service.MongoService;
-import uk.ac.ebi.literature.textminingapi.utility.Utility;
-
 @Component
 public class TextminingApiPublicValidatorImpl implements TextminingApiPublicValidator {
 	
 	private static final Logger logger = LoggerFactory.getLogger(TextminingApiPublicValidatorImpl.class);
 	private final MongoService mongoService;
-	
-	private final WebClient europePMCWebClientTemplate;
 
-	public TextminingApiPublicValidatorImpl(MongoService mongoService, WebClient europePMCWebClientTemplate) {
+	public TextminingApiPublicValidatorImpl(MongoService mongoService) {
 		this.mongoService = mongoService;
-		this.europePMCWebClientTemplate = europePMCWebClientTemplate;
 	}
 
 	@Override
@@ -44,7 +34,7 @@ public class TextminingApiPublicValidatorImpl implements TextminingApiPublicVali
 		
 		boolean valid = true;
 		
-		List<String> errors = new ArrayList<String>();
+		List<String> errors = new ArrayList<>();
 		
         if (obj==null) {
         	errors.add(NULL_ERROR);
@@ -57,16 +47,10 @@ public class TextminingApiPublicValidatorImpl implements TextminingApiPublicVali
         }
         
         if ((obj!= null) && (!Utility.isEmpty(obj.getFtId())) ) {
-        	if (!checkEpmc(obj.getFtId())) {
-        		errors.add(FT_ID_NOT_EXISTING_IN_EPMC_ERROR);
-        		valid = false;
-        	}
-        	
         	if (!checkMongoSubmission(obj, previousSubmission)) {
         		errors.add(SUBMISSION_ALREADY_EXISTING_ERROR);
         		valid = false;
         	}
-        	
         }
         
         if ((obj!= null) && (Utility.isEmpty(obj.getCallback()))) {
@@ -88,7 +72,7 @@ public class TextminingApiPublicValidatorImpl implements TextminingApiPublicVali
         	int index=1;
         	AtomicReference<List<String>> errorMessageFile;
         	for (FileInfo file : obj.getFiles()) {
-        		errorMessageFile = new AtomicReference<List<String>>();
+        		errorMessageFile = new AtomicReference<>();
         		if (!this.isValidFile(file, errorMessageFile, index)) {
         			errors.addAll(errorMessageFile.get());
                 	valid = false;
@@ -117,41 +101,8 @@ public class TextminingApiPublicValidatorImpl implements TextminingApiPublicVali
 		return Optional.empty();	
 	}
 	
-	protected boolean checkEpmc(String ftId) {
-		String query="FT_ID:"+ftId;
-		final String encodedQuery;
-		boolean valid=false;
-        
-		try {
-	        encodedQuery = URLEncoder.encode(query, "UTF8");
-	        
-	        EuropePMCLite responseApi = makeEPMCcall(encodedQuery);
-	        valid = responseApi.getHitCount() > 0;
-		}catch (Exception e) {
-			valid = false;
-			logger.error("Error in querying EPMC API for ft_id "+ftId, e);
-		}
-        
-        return valid;
-	}
-	
-	protected EuropePMCLite makeEPMCcall(String encodedQuery) throws Exception {
-		 return europePMCWebClientTemplate.get()
-	                .uri(uriBuilder -> {
-	                        return uriBuilder
-	                                .queryParam("format", "json")
-	                                .queryParam("query", encodedQuery)
-	                                .queryParam("resultType", "idlist")
-	                                .build();
-	                })
-	                .retrieve()
-	                .bodyToMono(EuropePMCLite.class)
-	                .retryWhen(Retry.fixedDelay(3, Duration.ofSeconds(5)))
-	                .block();
-	}
-	
 	private boolean isValidFile(FileInfo file , AtomicReference<List<String>> errorMessage, int index) {
-		List<String> errors = new ArrayList<String>();
+		List<String> errors = new ArrayList<>();
 		boolean valid=true;
 		if (Utility.isEmpty(file.getFilename())) {
 			errors.add(String.format(FILE_NAME_EMPTY_ERROR, index));
@@ -195,7 +146,7 @@ public class TextminingApiPublicValidatorImpl implements TextminingApiPublicVali
 	public boolean validateDeletion(String ftId, String user, AtomicReference<List<String>> errorMessages) {
 		boolean valid = true;
 		
-		List<String> errors = new ArrayList<String>();
+		List<String> errors = new ArrayList<>();
         
         if ((Utility.isEmpty(ftId))) {
         	errors.add(FT_ID_EMPTY_URL_PATH_DELETE_ERROR);
